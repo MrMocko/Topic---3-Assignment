0ea47d2409ad14ca73937b43c17b2c7552a2a1cd
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-12 09:39:29 -0400

    Add project files.

---
+++ Topic - 3 Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Topic___3_Assignment
+{ 
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            //Andre
+            int roomnumber = 29;
+            double price = 2.99;
+            string coursename = "Computer science";
+            Console.BackgroundColor = ConsoleColor.;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("This room is #" + roomnumber);
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine("i am learning about" + coursename);
+            Console.WriteLine();
+            string name = "Andre Mocko";
+            int gradYear = 2026; 
+            Console.WriteLine(" my name is " + name + " and i'll graduate in " + gradYear);
+            Console.WriteLine();
+            int ra = 6;
+            double area = Math.PI * Math.Pow(ra, 6);
+            Console.WriteLine();
+
+
+        }
+    }
+}
